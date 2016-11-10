ea37f0b4d3e793428705301a3771c916c5b8c141
codyguha <aalo.guha@gmail.com>
2016-11-09 16:00:21 -0800

    heroku

--- survey-chicken.js
+++ survey-chicken.js
@@ -288,17 +288,13 @@ bot.onTextMessage(/not at all|YES!$/i, (incoming, next) => {
         const message = Bot.Message.text(`Thanks for taking some time to chat with us.  We enjoyed learning more about your chicken preferences.  Please let us know what you thought of this survey by selecting an emoji that best represents your experience chatting with Survey Chicken`)
 		incoming.reply(message)
 		saveToMongoDb(user.username, incoming.body, "hunger")
-		bot.onTextMessage((incoming, next) => {
+		bot.onTextMessage((incoming) => {
 			const message = Bot.Message.text(`Thanks thats it. Say "hi" again sometime.`)
 			incoming.reply(message)
             saveToMongoDb(user.username, incoming.body, "emoji")
 		});
     });
 });
-
-bot.onTextMessage((incoming, next) => {
-			incoming.reply("end")
-		});
 
 // bot.onTextMessage(/YES!$/i, (incoming, next) => {
 //     bot.getUserProfile(incoming.from)
